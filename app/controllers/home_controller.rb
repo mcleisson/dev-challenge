978dab437f020aaee35088d5c0e8fdd85a7bf780
Marcel Pereira <mcleisson@gmail.com>
2021-03-09 00:31:30 -0300

    create front infos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+     @loans = Loan.all(current_user)
+     @credits = Credit.all(current_user)
   end
+
+  def method_name
+    
+  end
+  
 end
