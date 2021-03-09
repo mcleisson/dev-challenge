3037487fcac86a070fa1e51ec04faf7239a12c5c
Marcel Pereira <mcleisson@gmail.com>
2021-03-09 00:34:08 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  resources :loans
   resources :credits
   resources :users
   
+get 'loans/:id/show_calc', to: 'loans#show_calc'
+
   root to: 'home#index'
   
 end
